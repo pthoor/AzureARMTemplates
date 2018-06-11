dfad4e87b957e3a6fdec7e4eba5970bb17f85a8d
Pierre Thoor <pierre.thoor@addpro.se>
2018-06-11 06:58:11 +0200

    New DSC config

--- Azure Automation DSC/ADDSExistingDomain DSC/ADDSExistingDomain.ps1
+++ Azure Automation DSC/ADDSExistingDomain DSC/ADDSExistingDomain.ps1
@@ -1,55 +1,63 @@
 ﻿configuration ADDSExistingDomain
-{
-    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
-    Import-DscResource -ModuleName 'xActiveDirectory'
-    Import-DSCResource -ModuleName 'xPendingReboot'
+{ 
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration' 
+    Import-DscResource -ModuleName 'xActiveDirectory' 
+    Import-DSCResource -ModuleName 'xPendingReboot' 
     
-    $dscDomainName = Get-AutomationVariable -Name 'DomainName'
-    $InternalDomainControllerIP = Get-AutomationVariable -Name 'DCIPAddress'
-    $dscDomainAdmin = Get-AutomationPSCredential -Name 'DomainAdmin'
-    $SafeModeAdminCreds = Get-AutomationPSCredential -Name 'SafeModeAdminCreds'
-    $RetryCount = 10
-    $RetryIntervalSec = 15
+    $dscDomainName = Get-AutomationVariable -Name 'DomainName' 
+    $InternalDomainControllerIP = Get-AutomationVariable -Name 'DCIPAddress' 
+    $dscDomainAdmin = Get-AutomationPSCredential -Name 'DomainAdmin' 
+    $SafeModePassword = $dscDomainAdmin 
+    $dscDomainJoinAdminUsername = $dscDomainAdmin.UserName 
+    $dscDomainJoinAdmin = new-object -typename System.Management.Automation.PSCredential -argumentlist "$dscDomainName\$dscDomainJoinAdminUsername", $dscDomainAdmin.Password 
+    $RetryCount = 10 
+    $RetryIntervalSec = 15 
     
-    node "localhost" {
-
-            WindowsFeature RSAT 
-            {
-                  Ensure = "Present"
-                  Name = "RSAT"
-            }
-
-            WindowsFeature ADDS
-            {
-                   Ensure = "Present"
-                   Name   = "AD-Domain-Services"
-            }
-            
-            xWaitForADDomain DscForestWait 
-            { 
-                  DomainName = $dscDomainName 
-                  DomainUserCredential= $dscDomainAdmin
-                  RetryCount = $RetryCount
-                  RetryIntervalSec = $RetryIntervalSec
-                  DependsOn = "[WindowsFeature]ADDS"
-            }
-
-            xADDomainController ReplicaDC 
-            { 
-                 DomainName = $dscDomainName 
-                 DomainAdministratorCredential = $dscDomainAdmin 
-                 SafemodeAdministratorPassword = $SafeModeAdminCreds.Password
-                 DatabasePath = "C:\NTDS"
-                 LogPath = "C:\NTDS"
-                 SysvolPath = "C:\SYSVOL"
-                 DependsOn = "[xWaitForADDomain]DScForestWait"
-            }
-
-            xPendingReboot Reboot1
-            { 
-                Name = "RebootServer"
-                DependsOn = "[xADDomainController]ReplicaDC"
-            }
-    }
-
-}
+    node "localhost" { 
+        WindowsFeature RSAT 
+        { 
+            Ensure = "Present" 
+            Name = "RSAT" 
+        } 
+        WindowsFeature ADDS 
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services" 
+        } 
+        xWaitForADDomain DscForestWait 
+        { 
+            DomainName = $dscDomainName 
+            DomainUserCredential= $dscDomainJoinAdmin 
+            RetryCount = $RetryCount 
+            RetryIntervalSec = $RetryIntervalSec 
+            DependsOn = "[WindowsFeature]ADDS" 
+        } 
+        xADReplicationSubnet AzureSubnet 
+        { 
+            Ensure = "Present" 
+            Name = "10.0.1.0/16" 
+            Site = "AzureSite" 
+        } 
+        xADReplicationSite AzureSite 
+        { 
+            Ensure = "Present" 
+            Name = "AzureSite" 
+            DependsOn = "[xADReplicationSubnet]AzureSubnet" 
+        } 
+        xADDomainController ReplicaDC 
+        { 
+            DomainName = $dscDomainName 
+            DomainAdministratorCredential = $dscDomainAdmin 
+            SafemodeAdministratorPassword = $SafeModePassword 
+            DatabasePath = "C:\NTDS" 
+            LogPath = "C:\NTDS" 
+            SysvolPath = "C:\SYSVOL" 
+            DependsOn = "[xWaitForADDomain]DScForestWait" 
+        } 
+        xPendingReboot Reboot1 
+        { 
+            Name = "RebootServer" 
+            DependsOn = "[xADDomainController]ReplicaDC" 
+        } 
+    } 
+} 
